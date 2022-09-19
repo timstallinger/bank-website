b4fd850008951bcabbbd95632c1fb41fdbd35177
arjar <ararifi@uni-mainz.de>
2022-09-19 15:18:16 +0200

    Update

--- megagutebank/login/views.py
+++ megagutebank/login/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from django.template import loader
 
 template = loader.get_template('login.html')
+register_template = loader.get_template('register.html')
 
 def index(request):
     return HttpResponse(template.render({}, request))
     # return HttpResponse("Willkommen bei der Megagutebank Bank!")
+
+def register(request):
+    return HttpResponse(register_template.render({}, request))
