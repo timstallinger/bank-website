66f418906d4767038fb42ee6575fb2f191613490
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-19 13:13:21 +0200

    Homepage

--- megagutebank/home/views.py
+++ megagutebank/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
+template = loader.get_template('index.html')
+
+context ={
+    'home': '',
+}
 
 def index(request):
-    return HttpResponse("Willkommen bei der Megagutebank Bank!")
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("Willkommen bei der Megagutebank Bank!")
