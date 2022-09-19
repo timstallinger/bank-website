35c328f691a6959851b9d4af49ff90eb01aec1a0
arjar <ararifi@uni-mainz.de>
2022-09-19 14:27:12 +0200

    Update

--- megagutebank/home/models.py
+++ megagutebank/home/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Bank(models.Model):
+    bilanz = models.FloatField()
+    gewinn = models.FloatField()
+    bic = models.CharField(max_length=11, primary_key=1)
+    name = models.CharField(max_length=50)
