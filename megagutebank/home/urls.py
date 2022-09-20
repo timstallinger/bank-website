df82f465382f0b83d2f1ab01e91dc4c6c564e228
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-20 09:31:37 +0200

    Loginsystem

--- megagutebank/home/urls.py
+++ megagutebank/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
 ]
--- megagutebank/home/views.py
+++ megagutebank/home/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
-template = loader.get_template('index.html')
-
-context ={
-    'home': '',
-}
-
-def index(request):
-    return HttpResponse(template.render(context, request))
-    # return HttpResponse("Willkommen bei der Megagutebank Bank!")
