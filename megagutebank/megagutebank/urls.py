38742a2e88c87d868afc2b259fc92d20b8928734
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-20 11:09:59 +0200

    Header an Profilstatus angepasst

--- megagutebank/megagutebank/urls.py
+++ megagutebank/megagutebank/urls.py
@@ -19,7 +19,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="home"),
-    path("register/", include("home.urls")),
+    path("accounts/register/", include("home.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
