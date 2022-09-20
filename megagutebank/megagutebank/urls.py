655979dbf5fea4bd3c61afb303bf8f5cba4c6de2
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-20 10:01:38 +0200

    Login Logout Working

--- megagutebank/megagutebank/urls.py
+++ megagutebank/megagutebank/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', include('home.urls')),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
