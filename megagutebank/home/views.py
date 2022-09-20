310d5d992dc7dc42cc57f8a2c9b814ef51e3cb7f
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-20 11:02:45 +0200

    registrierungsseite

---
+++ megagutebank/home/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
