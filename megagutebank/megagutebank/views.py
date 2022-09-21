2cfc4c12dc0118dbacdc29238d5f246df72537f8
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-21 14:36:42 +0200

    Fix unauthenticated user

--- megagutebank/megagutebank/views.py
+++ megagutebank/megagutebank/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 def index(request):
     kontostand = 0
     # sum up all accounts of the user
-    for account in request.user.account_set.all():
-        kontostand += account.amount
+    if request.user.is_authenticated:
+        for account in request.user.account_set.all():
+            kontostand += account.amount
     return render(request, 'index.html', {'user': request.user, 'kontostand': kontostand})
