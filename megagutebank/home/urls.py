2665d182fe17a6a6065398643d01a38c964aebdf
Tim Klaus Stallinger <tistalli@students.uni-mainz.de>
2022-09-23 12:43:06 +0200

    transactions table

--- megagutebank/home/urls.py
+++ megagutebank/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path('register/', views.signup, name='signup'),
+    path('transactions/', views.transactions, name='transactions'),
     path('konto/erstellen', views.konto_create, name='konto_create'),
     path('profile/', views.profile_data, name="user_profile"),
     path('konto/uberweisen', views.konto_uberweisen, name='konto_uberweisen'),
